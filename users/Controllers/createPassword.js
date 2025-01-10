9181d9bdd9c7309f5ec28d0312313ed276ac35ec
shristy verma <meherv862@gmail.com>
2025-01-10 14:33:57 +0530

    "Initial Commit"

--- users/Controllers/createPassword.js
+++ users/Controllers/createPassword.js
@@ -3,36 +3,38 @@ import TemporaryUser from '../models/temporaryUserModal.js';
 import User from '../models/user.js';
 
 export const createPassword = async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, confirmPassword } = req.body;
 
   try {
-    // Find the temporary user by email
+    // Check if passwords match
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: 'Passwords do not match' });
+    }
+
     const temporaryUser = await TemporaryUser.findOne({ email });
 
     if (!temporaryUser) {
       return res.status(400).json({ message: 'Temporary user not found' });
     }
+
     // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Create a new user with the data from the temporary user
     const newUser = new User({
       email: temporaryUser.email,
       password: hashedPassword,
       rawPassword: password, 
       userType: temporaryUser.userType,
-      isVerified: true,  
+      isVerified: true,
     });
 
-    // Save the new user
     await newUser.save();
 
-    // Delete the temporary user
     await TemporaryUser.deleteOne({ email });
 
     return res.status(200).json({ message: 'User created successfully and moved to Database' });
   } catch (error) {
-    console.error('Error in createPasswordAndMoveData:', error.message);
+    console.error('Error in createPassword:', error.message);
     return res.status(500).json({ message: `Server error: ${error.message}` });
   }
 };
