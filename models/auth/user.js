13d5abf716340d1bb41647c0603220a7210956be
shristy verma <meherv862@gmail.com>
2025-01-21 14:13:08 +0530

    "fix creating password "

--- models/auth/user.js
+++ models/auth/user.js
@@ -1,19 +1,28 @@
 import mongoose from 'mongoose';
-import { ROLES } from '../../constants/role.js'; 
+import { ROLES } from '../../constants/role.js';
 
-const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    phone: { type: String, required: true },
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String },
+    phone: { type: String },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     rawPassword: { type: String, required: true }, // Store unhashed password for temporary use
     status: { type: String, default: 'pending' }, // Pending status by default
-    serviceProvider: { type: mongoose.Schema.Types.ObjectId, ref: 'ServiceProvider', required: true }, // Reference to ServiceProvider
+    serviceProvider: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'ServiceProvider',
+      required: function () {
+        return this.userType === 'serviceProvider'; // Required only if userType is 'serviceProvider'
+      },
+    },
     userType: { type: String, required: true, enum: Object.values(ROLES) }, // Role of the user
     isVerified: { type: Boolean, default: false }, // Email/Account verification status
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // User who created this account
     profile: { type: mongoose.Schema.Types.ObjectId, ref: 'Profile' }, // Link to user profile
     addedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // User who added this user (if applicable)
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model('User', userSchema);
